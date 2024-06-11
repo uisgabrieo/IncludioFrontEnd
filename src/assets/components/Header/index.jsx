94e808c8c6ac28dfd013cce22437e00118451679
Luis Gabriel <barros.luissalv@gmail.com>
2024-06-11 01:43:21 -0300

    Header Estilizado

--- src/assets/components/Header/index.jsx
+++ src/assets/components/Header/index.jsx
@@ -5,10 +5,10 @@ function Header() {
     return (
         //<StyledHeader>
         <header className={styles.header}>
-            <a href="index.html"><img src="./assets/img/global/Logo_Site_Autismo.png" alt="Logo Includio" id="logo"/></a>
+            <a href="index.html"><img src="../../../../public/img/global/Logo_Site_Autismo.png" alt="Logo Includio" className={styles.logo}/></a>
             <div id="divBotoes">
-                <button class="botao" onclick="window.location.href='./assets/pages/global/login.html'">Login</button>
-                <button class="botao" onclick="window.location.href='./assets/pages/global/registroInicial.html'">Sing Up</button>
+                <button className={styles.botao} onClick={() => window.location.href='./assets/pages/global/login.html'}>Login</button>
+                <button className={styles.botao} onClick={() => window.location.href='./assets/pages/global/registroInicial.html'}>Sing Up</button>
             </div>
         </header>
         //</StyledHeader>
