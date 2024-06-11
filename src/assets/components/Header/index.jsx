faea6d8ded2f882df419190bfe3a3aa0a644ce39
Luis Gabriel <barros.luissalv@gmail.com>
2024-06-11 15:43:00 -0300

    Pagina Home completa

--- src/assets/components/Header/index.jsx
+++ src/assets/components/Header/index.jsx
@@ -5,15 +5,18 @@ function Header() {
     return (
         //<StyledHeader>
         <header className={styles.header}>
-            <a href="index.html"><img src="../../../../public/img/global/Logo_Site_Autismo.png" alt="Logo Includio" className={styles.logo}/></a>
+            <a href="index.html"><img src="../../../../public/img/global/Logo_Site_Autismo.png" alt="Logo Includio" className={styles.logo} /></a>
             <nav className={styles.navegacao}>
+                <div>
                 <a href="" className={styles.link}>Sobre</a>
                 <a href="" className={styles.link}>Entenda Sobre o Autismo</a>
+                </div>
+
+                <div className={styles.divBotoes}>
+                    <button className={styles.botao} onClick={() => window.location.href = './assets/pages/global/login.html'}>Login</button>
+                    <button className={styles.botao} onClick={() => window.location.href = './assets/pages/global/registroInicial.html'}>Sing Up</button>
+                </div>
             </nav>
-            <div className={styles.divBotoes}>
-                <button className={styles.botao} onClick={() => window.location.href='./assets/pages/global/login.html'}>Login</button>
-                <button className={styles.botao} onClick={() => window.location.href='./assets/pages/global/registroInicial.html'}>Sing Up</button>
-            </div>
         </header>
         //</StyledHeader>
     )
