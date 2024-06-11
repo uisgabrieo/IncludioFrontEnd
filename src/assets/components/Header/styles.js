7c7aebdcc4c3e392f1bbab329a3696a75ac4e284
Luis Gabriel <barros.luissalv@gmail.com>
2024-06-11 01:03:08 -0300

    Nome na tela

---
+++ src/assets/components/Header/styles.js
@@ -0,0 +1,9 @@
+import { styled } from "@stitches/react";
+
+const StyledHeader = styled("header", {
+    marginTop: "20px",
+    height: "120px",
+    display: "flex",
+    alignItems: "center",
+    backgroundColor: "#012340"
+});
