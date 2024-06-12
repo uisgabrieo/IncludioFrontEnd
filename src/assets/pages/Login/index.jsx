17793d1a784bd2a9db20184ac38079ff3b5d4635
Luis Gabriel <barros.luissalv@gmail.com>
2024-06-11 21:30:16 -0300

    Pagina Login

--- src/assets/pages/Login/index.jsx
+++ src/assets/pages/Login/index.jsx
@@ -1,48 +1,52 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
 import React from "react";
 import Header from "../../components/Header";
-import Container from "../../components/Container";
-import Footer from "../../components/Footer";
-import styles from "./Login.module.css"
+import styles from "./Login.module.css";
+import { envioDados, mostrarSenha } from "../../utils/Login/login";
 
 function Login() {
-    const mostrarSenha = () => {
-        // Adicione a lógica para mostrar a senha aqui
-    };
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+    const [erro, setErro] = useState("");
 
     return (
         <>
             <Header />
-            <Container>
-                <main className="main">
-                    <section className={styles.cardLogin}>
-                        <h1>Entrar</h1>
-                        <form action="" method="post" className={styles.dadosLogin}>
-                            <div>
-                                <label htmlFor="email">
-                                    <div>
-                                        Email<br />
-                                        <input type="email" name="email" id={styles.email} className={styles.credenciais} />
+            <main className={styles.main}>
+                <section className={styles.cardLogin}>
+                    <h1>Entrar</h1>
+                    <form onSubmit={(e) => envioDados(e, email, senha, setErro)} id="loginPage">
+                        <div>
+                            <label htmlFor="email">
+                                <div>
+                                    Email<br />
+                                    <input type="email" name="email" id="email" className={styles.credenciais} placeholder="exemplo@gmail.com" onChange={(e) => setEmail(e.target.value)}
+                                    />
+                                </div>
+                            </label>
+                        </div>
+                        <div>
+                            <label htmlFor="senha">
+                                <div className={styles.caixaSenha}>
+                                    Senha<br />
+                                    <div className={styles.campoSenha}>
+                                        <input type="password" name="senha" id="senha" className={styles.credenciais} placeholder="Senha" onChange={(e) => setSenha(e.target.value)} />
+                                        <i className={`bi bi-eye-fill ${styles.olho}`} id="olho" onClick={mostrarSenha}></i>
                                     </div>
-                                </label>
-                            </div>
-                            <div>
-                                <label htmlFor="password">
-                                    <div id={styles.caixaSenha}>
-                                        Password<br />
-                                        <input type="password" name="password" id={styles.password} className={styles.credenciais} />
-                                        <i className={`bi bi-eye-fill ${styles.botaoSenha}`} onClick={mostrarSenha}></i>
-                                    </div>
-                                </label>
-                            </div>
-                            <div>
-                                <button type="submit" className={styles.btnConfirme}>Login</button>
-                            </div>
-                        </form>
-                        <p id={styles.mensagemErro} style={{ color: "red" }}></p>
-                    </section>
-                </main>
-            </Container>
-            <Footer />
+                                </div>
+                            </label>
+                        </div>
+                        <p className={styles.mensagemErro} style={{ color: "red" }}>{erro}</p>
+                        <div>
+                            <button type="submit" className={styles.btnConfirme}>Login</button>
+                        </div>
+                    </form>
+                </section>
+                <section className="registre">
+                    <p>Não possui cadastro? <Link to="/registro" className={styles.registroLink}>Registre-se aqui!</Link></p>
+                </section>
+            </main>
         </>
     );
 }
---
+++ src/assets/utils/Login/login.js
@@ -0,0 +1,46 @@
+export function envioDados(event, email, senha, setErro) {
+    event.preventDefault();
+
+    const login = {
+        email: email,
+        password: senha
+    };
+
+    fetch("http://localhost:8080/api/account/login", {
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        method: "POST",
+        body: JSON.stringify(login)
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.text();
+        } else {
+            throw new Error('Credenciais inválidas');
+        }
+    })
+    .then(data => {
+        console.log(data); 
+        localStorage.setItem('accountResponse', JSON.stringify(data));
+        window.location.href = '../global/homePage.html';
+    })
+    .catch(error => {
+        console.log("Erro: " + error.message);
+        setErro("Credenciais inválidas");
+    });
+}
+
+export function mostrarSenha() {
+    const senha = document.getElementById("senha");
+    const exporSenha = document.getElementById("olho");
+
+    if (senha.type === "password") {
+        senha.setAttribute("type", "text");
+        exporSenha.classList.replace("bi-eye-fill", "bi-eye-slash-fill");
+    } else {
+        senha.setAttribute("type", "password");
+        exporSenha.classList.replace("bi-eye-slash-fill", "bi-eye-fill");
+    }
+}
